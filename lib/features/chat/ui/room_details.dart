2e6ac6853d58e6ee07a02584a26717be350bf682
SIDDHARTH K P <siddhubhai998@gmail.com>
2025-12-04 01:01:09 +0530

    Create AppBar with title and two iconButtons. The leading iconButton should behave like a back button (Will implement later). The "more details" button is not connected to anything currently.

--- lib/features/chat/ui/room_details.dart
+++ lib/features/chat/ui/room_details.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: ChatDetailsScreen(),
-  ));
+  runApp(const MaterialApp(home: ChatDetailsScreen()));
 }
 
 class ChatDetailsScreen extends StatelessWidget {
@@ -13,6 +11,35 @@ class ChatDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            /*Go back to the previous screen*/
+            ;
+          },
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+        ),
+        title: const Text(
+          'Details',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 23.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              /*More details*/
+              ;
+            },
+            icon: Icon(Icons.more_vert, color: Colors.black),
+          ),
+        ],
+      ),
       body: Container(),
     );
   }
