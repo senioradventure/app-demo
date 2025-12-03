de4ffead6dff609f34c68bd18db7d4291dbbcb3b
SIDDHARTH K P <siddhubhai998@gmail.com>
2025-12-04 03:45:04 +0530

    Disabled the debug banner and commented out the main function. Features pending : - Tags under the pfp

--- lib/features/chat/ui/room_details.dart
+++ lib/features/chat/ui/room_details.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(home: ChatDetailsScreen()));
-}
+/*void main() {
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatDetailsScreen()
+  )
+  );
+}*/
 
 const name = "Chai Talks";
 
