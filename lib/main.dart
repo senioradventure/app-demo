3c6fc18d1cb841793ee101cfa5535ea6c5ae3ce3
Sheens13 <sheness35@gmail.com>
2025-12-05 10:32:06 +0530

    added main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:senior_circle/features/home/home_page.dart';
+import 'package:senior_circle/features/livechat/ui/livechat_page.dart';
 
 void main() {
+  filteredContactList.value = List.from(masterContactList);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Senior Circle',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(),
+      // show the page wid
+      //get (do NOT use MyApp() here)
+      
+      home: const LiveChatPage(),
     );
   }
 }
