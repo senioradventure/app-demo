abed0f9b68bfb4a2d8cccced20762145b6f2fe21
Abhijith C <chalilabhijith123@gmail.com>
2025-12-27 17:37:50 +0530

    code formatted - Abhi

--- lib/features/live_chat_home/presentation/live_chat_home_page.dart
+++ lib/features/live_chat_home/presentation/live_chat_home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:senior_circle/core/constants/strings/lists.dart';
 import 'package:senior_circle/features/createroom/presentation/create_room_screen.dart';
 import 'package:senior_circle/features/live_chat_home/presentation/bloc/live_chat_home_bloc.dart';
-import 'package:senior_circle/features/live_chat_home/presentation/repository/live_chat_home_repo.dart';
 import 'package:senior_circle/features/live_chat_home/presentation/widget/app_bar.dart';
 import 'package:senior_circle/features/live_chat_home/presentation/widget/contact_list.dart';
 import 'package:senior_circle/features/live_chat_home/presentation/widget/floating_button.dart';
@@ -17,115 +16,104 @@ class LiveChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider(
-  create: (_) => LiveChatHomeRepository(),
-  child: BlocProvider(
-    create: (context) => LiveChatHomeBloc(
-      context.read<LiveChatHomeRepository>(),
-    )
-      ..add(FetchLocationsEvent())
-      ..add(FetchRoomsEvent()),
-    child: Scaffold(
-        backgroundColor: const Color(0xFFF9F9F7),
-        body: SafeArea(
-          child: Container(
-            color: const Color(0xFFF9F9F7),
+    return Scaffold(
+      backgroundColor: const Color(0xFFF9F9F7),
+      body: SafeArea(
+        child: Container(
+          color: const Color(0xFFF9F9F7),
 
-            
-            child: BlocBuilder<LiveChatHomeBloc, LiveChatHomeState>(
-              builder: (context, state) {
-                return Column(
-                  children: [
-                    const PageHeader(title: 'Live Chat'),
+          child: BlocBuilder<LiveChatHomeBloc, LiveChatHomeState>(
+            builder: (context, state) {
+              return Column(
+                children: [
+                  const PageHeader(title: 'Live Chat'),
 
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 22,
-                        vertical: 4,
-                      ),
-                      child: SearchTextField(
-                        onChanged: (txt) {
-                          context.read<LiveChatHomeBloc>().add(
-                            UpdateSearchEvent(txt),
-                          );
-                        },
-                      ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 22,
+                      vertical: 4,
                     ),
-
-                    const SizedBox(height: 8),
-
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 22),
-                      child: Row(
-                        children: [
-                          LocationFilterButton(
-                            locations: state.locations,
-                            selectedLocation: state.selectedLocation,
-                            onLocationSelected: (locId) {
-                              context.read<LiveChatHomeBloc>().add(
-                                UpdateLocationFilterEvent(locId),
-                              );
-                            },
-                          ),
-
-                          const SizedBox(width: 15),
-
-                          InterestFilterButton(
-                            interests: AppLists.interests,
-                            selectedInterest: state.selectedInterest,
-                            onInterestSelected: (interest) {
-                              context.read<LiveChatHomeBloc>().add(
-                                UpdateInterestFilterEvent(interest),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
+                    child: SearchTextField(
+                      onChanged: (txt) {
+                        context.read<LiveChatHomeBloc>().add(
+                          UpdateSearchEvent(txt),
+                        );
+                      },
                     ),
+                  ),
 
-                    const SizedBox(height: 16),
+                  const SizedBox(height: 8),
 
-                    Expanded(
-                      child: Container(
-                        color: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 22),
+                    child: Row(
+                      children: [
+                        LocationFilterButton(
+                          locations: state.locations,
+                          selectedLocation: state.selectedLocation,
+                          onLocationSelected: (locId) {
+                            context.read<LiveChatHomeBloc>().add(
+                              UpdateLocationFilterEvent(locId),
+                            );
+                          },
+                        ),
 
-                        child: ContactRoomList(roomList: state.filteredRooms),
-                      ),
+                        const SizedBox(width: 15),
+
+                        InterestFilterButton(
+                          interests: AppLists.interests,
+                          selectedInterest: state.selectedInterest,
+                          onInterestSelected: (interest) {
+                            context.read<LiveChatHomeBloc>().add(
+                              UpdateInterestFilterEvent(interest),
+                            );
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                );
-              },
-            ),
-          ),
-        ),
+                  ),
 
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(right: 23, bottom: 17),
-          child: AppFAB(
-            onPressed: () async {
-              final result = await Navigator.push<Map<String, dynamic>?>(
-                context,
-                MaterialPageRoute(builder: (_) => const CreateRoomScreen()),
+                  const SizedBox(height: 16),
+
+                  Expanded(
+                    child: Container(
+                      color: Colors.white,
+
+                      child: ContactRoomList(roomList: state.filteredRooms),
+                    ),
+                  ),
+                ],
               );
-
-              if (!context.mounted) return;
-
-              if (result != null) {
-                await Supabase.instance.client.from('live_chat_rooms').insert({
-                  'name': result['roomName'],
-                  'image_url': result['imageUrl'],
-                  'interests': [],
-                });
-
-                if (!context.mounted) return;
-
-                context.read<LiveChatHomeBloc>().add(FetchRoomsEvent());
-              }
             },
           ),
         ),
       ),
-    )
+
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(right: 23, bottom: 17),
+        child: AppFAB(
+          onPressed: () async {
+            final result = await Navigator.push<Map<String, dynamic>?>(
+              context,
+              MaterialPageRoute(builder: (_) => const CreateRoomScreen()),
+            );
+
+            if (!context.mounted) return;
+
+            if (result != null) {
+              await Supabase.instance.client.from('live_chat_rooms').insert({
+                'name': result['roomName'],
+                'image_url': result['imageUrl'],
+                'interests': [],
+              });
+
+              if (!context.mounted) return;
+
+              context.read<LiveChatHomeBloc>().add(FetchRoomsEvent());
+            }
+          },
+        ),
+      ),
     );
   }
 }
