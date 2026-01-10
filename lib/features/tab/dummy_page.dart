19f5f33920bd154528f4f7b7791c3f133eb13587
SIDDHARTH K P <siddhubhai998@gmail.com>
2026-01-10 15:22:41 +0530

    Minor fixes

--- lib/features/tab/dummy_page.dart
+++ lib/features/tab/dummy_page.dart
@@ -5,6 +5,6 @@ class DummyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Comming soon ....."));
+    return const Center(child: Text("Coming soon ....."));
   }
 }
