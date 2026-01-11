a785db64e494770ccb93582c6e64b6d3d468c372
nidha-rahma <nidharahma373@gmail.com>
2026-01-12 01:13:40 +0530

    time

--- lib/core/utils/time_utils.dart
+++ lib/core/utils/time_utils.dart
@@ -1,14 +1,20 @@
 import 'package:intl/intl.dart';
+import 'package:flutter/foundation.dart';
 
 class TimeUtils {
   static String formatTimeString(String rawTime) {
     try {
+      if (rawTime.isEmpty) return '';
+      String utcTime = rawTime;
+      if (!rawTime.endsWith('Z') && !rawTime.contains('+')) {
+        utcTime = '${rawTime}Z';
+      }
 
-      DateTime dateTime = DateTime.parse(rawTime).toLocal();
-
+      DateTime dateTime = DateTime.parse(utcTime).toLocal();
+      debugPrint('🕒 TimeUtils: raw="$rawTime" -> utc="$utcTime" -> local="$dateTime"');
       return DateFormat('h:mm a').format(dateTime); 
     } catch (e) {
-
+      debugPrint('Error parsing time: $e');
       return rawTime;
     }
   }
